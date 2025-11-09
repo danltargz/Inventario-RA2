792c8a22e4478a9fc6bc5c9a0ea0e11a4e503ed8
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-11-08 22:27:50 -0300

    Função para adicionar item no inventário

--- InventarioLogica.hs
+++ InventarioLogica.hs
@@ -10,3 +10,14 @@ import qualified Data.Map as Map
 import Data.Time (UTCTime)
 
 type ResultadoOperacao = (Inventario, LogEntry)
+
+addItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao
+addItem tempo item inv
+  | Map.member (itemID item) inv =
+      Left "Erro: item já existe no inventário."
+  | otherwise =
+      let novoInv = Map.insert (itemID item) item inv
+          logEntry = LogEntry tempo Add
+            ("Item adicionado: " ++ nome item ++ " (ID: " ++ itemID item ++ ")")
+            Sucesso
+      in Right (novoInv, logEntry)            
